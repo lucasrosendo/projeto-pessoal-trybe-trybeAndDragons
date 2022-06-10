3b05dcf3c9b15f5ac7c1d9f7573ddd18c7177572
lucasrosendo <lucasrosendo91@gmail.com>
2022-06-09 21:01:13 -0300

    Req 1

---
+++ src/Races/Race.ts
@@ -0,0 +1,25 @@
+abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  public abstract get maxLifePoints(): number;
+
+  public static createdRacesInstances(): number {
+    throw Error('Not implemented');
+  }
+}
+
+export default Race;
